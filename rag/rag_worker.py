ca3d84452ee93867bff97509af98af1aa88de281
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-03-03 12:45:44 +0530

    retrival model

--- rag/rag_worker.py
+++ rag/rag_worker.py
@@ -1,13 +1,14 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from dotenv import load_dotenv
+import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 load_dotenv()
-pdf_path = "/Users/eklavyapopli/smart email assistant/rag/companyData.pdf"
+pdf_path = ""
 loader = PyPDFLoader(pdf_path)
 docs = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=30)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 
 chunks = text_splitter.split_documents(documents=docs)
 
@@ -16,7 +17,7 @@ embeddingModel = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
 
 vector_store = QdrantVectorStore.from_documents(
     documents=chunks,
-    url="http://localhost:6333",
+    url=os.getenv("QDRANTDB_URI"),
     collection_name="email_context",
     embedding=embeddingModel,
 )
